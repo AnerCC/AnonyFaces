b62fd3083aac0e9d38d17c180efd5a8fb78606fb
AnerCC <anercarmi@gmail.com>
2024-06-23 10:03:31 +0300

    first commit

---
+++ app/app.py
@@ -0,0 +1,124 @@
+from flask import Flask, request, jsonify
+from moduls import blureFace_dir,blureFace_file,blureFace_bytes_file
+import time
+import cv2 as cv
+import numpy as np
+from logger import create_logger
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/blur-file', methods=['POST'])
+def blur_file_bytes():
+  st=time.time()
+  try:
+    LOGGER = create_logger("blur_log")
+    LOGGER.info(f"blur-file request received")
+    print(f"blur-file request received")
+
+    # Access request data
+    srd=time.time()
+    LOGGER.info('reading request data')
+    print('reading request data')
+    image = request.files["image"]
+    byte_image=image.read()
+    fd_threshold=float(request.form.get("fd_threshold"))
+    print(f'finished reading data in -{time.time()-srd}')
+    sed=time.time()
+    print(f'tarted encoding image')
+    np_images=cv.imdecode(np.frombuffer(byte_image, np.uint8), cv.IMREAD_COLOR)
+    print(f'finished encoding image in - {time.time()-sed}')
+    blurred_image_np= blureFace_bytes_file(np_images,fd_threshold,LOGGER)  # Pass fd_threshold and logger
+    if blurred_image_np.any():
+      
+      shape=blurred_image_np.shape
+      ###for ndArray
+      flatten_blurre_images=blurred_image_np.ravel().tolist()
+      print(f'request handeled in {time.time()-st} seconds' )
+      return jsonify({'blurred_image': flatten_blurre_images,"shape":shape}), 200  # Return JSON with processed images
+      
+    else:
+      return '', 204  # No data to process
+   
+   
+  except Exception as e:
+    LOGGER.error(f"Error processing images: {e}")
+    print(f"Error processing images: {e}")
+    return jsonify({'error': str(e)}), 500
+
+
+
+@app.route('/blur-dir', methods=['POST'])
+def blur():
+    LOGGER = create_logger("blur_log")
+    LOGGER.info(f"blur-dir-path request recived")
+    if request.is_json:
+        fd_threshold=request.data["df_threshold"]
+        data = request.get_json()
+        print(f'Received data: {data}')
+        car_directory = data.get('car directory')
+        LOGGER.info(f"{car_directory} is  being sent or blurring")
+        blurred_images = blureFace_dir(car_directory,fd_threshold,LOGGER)
+        LOGGER.info(f"{car_directory} has done blurring proccess")
+        #blur_status options - blurred, no_detections, 
+        return blurred_images.tolist(),200
+
+
+# @app.route('/blur-file-deprecated', methods=['POST'])
+# def blur_file():
+#   try:
+#     LOGGER = create_logger("blur_log")
+#     LOGGER.info(f"blur-file request received")
+
+#     # Access request data
+#     data = request.json
+#     fd_threshold = data["fd_threshold"]
+#     image=data["image"]
+#     shape=data["shape"]
+#     reshaped_image = np.array(image,dtype=np.uint8).reshape(shape)
+#     blurred_image = blureFace_file(reshaped_image,fd_threshold,LOGGER)  # Pass fd_threshold and logger
+#     flatten_blurred_image=blurred_image.ravel().tolist()
+#     return jsonify({'blurred_image': flatten_blurred_image}), 200  # Return JSON with processed images
+  
+#   except Exception as e:
+#     LOGGER.error(f"Error processing images: {e}")
+#     return jsonify({'error': str(e)}), 500
+  
+
+
+
+
+
+
+@app.route('/blur-test-local')
+def blur_test_local():
+        LOGGER = create_logger("blur_log")
+        images=[]
+        LOGGER.info(f"blurring test request recived")
+        print(f'blur request recived')
+        car_directory = "images_to_blur"
+        images=[f'{car_directory}/1.jpg',f'{car_directory}/2.jpg']
+        for img in images:
+             images.append(cv.imread(f'{car_directory}/{img}'))
+        LOGGER.info(f"proccess started")
+        blurred_images = blureFace_file(images,0.4,LOGGER)
+        LOGGER.info(f"proccess ended")
+        print(f'detected faces in {len(blurred_images)} images' )
+        LOGGER.info(f'blur proccess ended blurred {len(blurred_images)} images')
+        return  f'blur proccess ended blurred {len(blurred_images)} images'
+    
+
+
+
+@app.route('/isAlive',methods=['GET','POST'])
+def is_alive():
+    if request.method=="GET":
+        return 'server is alive, "GET" request recived'
+    if request.method=="POST":
+        return 'server is alive, "POST" request recived'
+ 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ app/moduls/blur.py
@@ -0,0 +1,275 @@
+from retinaface import RetinaFace
+import cv2 
+import numpy as np
+import os
+import time
+
+
+# https://sefiks.com/2021/04/27/deep-face-detection-with-retinaface-in-python/
+
+
+COMPRESSION_LEVEL=99
+QUALITY=99
+
+
+def blureFace_dir(car_diretory,fd_threshold,LOGGER):
+
+    faces=[]
+    flvl=os.listdir(f'{car_diretory}')
+    for dir in flvl:
+        
+        dir_path = os.path.join(car_diretory, f'{dir}/1')
+        images=os.listdir(dir_path)
+        i=1
+        for img in images:
+            blurred_images=[]
+            img_path=os.path.join(dir_path,img)
+            
+            if os.path.isfile(f'{img_path}'):
+
+                    image=cv2.imread(img_path)
+                    LOGGER.info('face detection started')
+                    resp = RetinaFace.detect_faces(image,threshold=fd_threshold)
+                    LOGGER.info('face detection ended')
+                    i+=1
+                    for identity in resp:
+                        LOGGER.info('image blurring started')
+                        try:
+                            facial_area = resp[identity]['facial_area']   
+                            blurred=cv2.GaussianBlur(image,(71,71),500)
+                            faces.append(facial_area)
+
+                        except KeyError:
+                            LOGGER.info(f"KeyError: The key '{identity}' or 'facial_area' is missing in the response.")
+                           
+
+                        except cv2.error as e:
+                            LOGGER.info(f"OpenCV error occurred: {e}")
+                            
+                        except FileNotFoundError as e:
+                            LOGGER.info(f"File not found error: {e}")
+                            
+                        except Exception as e:
+                            LOGGER.info(f"An unexpected error occurred: {e}")
+                            
+                              
+                        try:
+                            # Get detected face area from retinaFace response and assign face region to face veriable
+                            x1, y1, x2, y2 = facial_area
+                        
+                            face = image[y1:y2, x1:x2]
+
+                            #blur the face region
+                            
+                            # Create circular mask of the same size as the blurred image
+                            mask = np.zeros_like(blurred)
+                            h, w = face.shape[:2]
+                            center = ((x1+w//2), (y1+h//2))
+                            
+                            cv2.ellipse(mask,center,(w//2,h//2),0,0,360, (255, 255, 255), -1)
+                            
+                            #create a circular blurred section
+                            blurred_circular = cv2.bitwise_and(blurred, mask)
+                            
+                            
+                            #apply the blurred circular section into the detected face area
+                            blurredFace=np.where(mask!=0,blurred_circular,image)
+
+                            #apply the blured face into the sorce image
+                            image[y1:y2, x1:x2]=blurredFace[y1:y2, x1:x2]
+                            
+                            # if not os.path.exists(f'{BLURRED_DIR}/{dir}'):
+                            #     os.mkdir(f'{BLURRED_DIR}/{dir}')
+                            # cv2.imwrite(f'{BLURRED_DIR}/{dir}/{img}', image)
+                            blurred_images.append(image)
+                            LOGGER.info('image blurring ended')
+                       
+                        except cv2.error as e:
+                            LOGGER.error(f"OpenCV error occurred: {e}")
+                            # Handle specific OpenCV errors
+
+                        except IndexError as e:
+                            LOGGER.error(f"IndexError: {e}")
+                            # Handle IndexError if facial_area doesn't have expected values
+
+                        except Exception as e:
+                            LOGGER.error(f"An unexpected error occurred: {e}")
+                            # Handle any other unexpected errors
+                        
+    if blurred_images !=[]:
+            return blurred_images
+    else:
+            return None
+
+# images = ndarrays of images, fd_threshold =  face detection threshold (float), LOGGER = Logger object for creating log
+def blureFace_file(image,fd_threshold,LOGGER):
+        st=time.time()
+        blurred_images=[]
+        indexes=[]
+        # for i,img in enumerate(images):
+                
+        LOGGER.info(f'face detection started ')
+        print(f'face detection started - {time.time()-st}')
+        resp = RetinaFace.detect_faces(image,threshold=fd_threshold)
+        LOGGER.info(f'face detection ended  ')
+        print(f'face detection ended  - {time.time()-st}')
+    
+        for identity in resp:
+            LOGGER.info(f'image blurring started ')
+            print(f'image blurring started - {time.time()-st} ')
+            try:
+                facial_area = resp[identity]['facial_area']   
+                blurred=cv2.GaussianBlur(image,(71,71),500)
+
+            except KeyError as e:
+                # Handle KeyError (e.g., if 'facial_area' or 'identity' is not found in resp)
+                LOGGER.error(f'KeyError: {str(e)}')
+
+            except cv2.error as e:
+                # Handle OpenCV errors (e.g., if there's an issue with image processing)
+                LOGGER.error(f'OpenCV error: {str(e)}')
+
+            except Exception as e:
+                # Handle other unexpected errors
+                LOGGER.error(f'Unexpected error: {str(e)}')
+
+            try:     
+                # Get detected face area from retinaFace response and assign face region to face veriable
+                x1, y1, x2, y2 = facial_area
+            
+                face = image[y1:y2, x1:x2]
+
+                #blur the face region
+                
+                # Create circular mask of the same size as the blurred image
+                mask = np.zeros_like(blurred)
+                h, w = face.shape[:2]
+                center = ((x1+w//2), (y1+h//2))
+                
+                cv2.ellipse(mask,center,(w//2,h//2),0,0,360, (255, 255, 255), -1)
+                # cv2.circle(mask, center, radius, (255, 255, 255), -1)
+                # cv2.imwrite("mask.jpg",mask)
+                #create a circular blurred section
+                blurred_circular = cv2.bitwise_and(blurred, mask)
+                
+                
+                #apply the blurred circular section into the detected face area
+                blurredFace=np.where(mask!=0,blurred_circular,image)
+
+                #apply the blured face into the sorce image
+                image[y1:y2, x1:x2]=blurredFace[y1:y2, x1:x2]
+            
+            except cv2.error as e:
+                LOGGER.error(f"OpenCV error occurred: {e}")
+                # Handle specific OpenCV errors
+
+            except IndexError as e:
+                LOGGER.error(f"IndexError: {e}")
+                # Handle IndexError if facial_area doesn't have expected values
+
+            except Exception as e:
+                LOGGER.error(f"An unexpected error occurred: {e}")
+                # Handle any other unexpected errors   
+        # blurred_images.append(image)
+        # indexes.append(i) 
+        LOGGER.info(f'image blurring ended for for image ')
+        print(f'image blurring ended for for image - {time.time()-st} ')
+
+        
+        return image      
+
+
+
+
+def blureFace_bytes_file(img,fd_threshold,LOGGER):
+        blurred_images=[]
+        indexes=[]
+        
+        
+        st=time.time()        
+        LOGGER.info(f'face detection started ')
+        print(f'face detection started - {st} ')
+        resp = RetinaFace.detect_faces(img,threshold=fd_threshold)
+        LOGGER.info(f'face detection ended  ')
+        print(f'face detection ended - {time.time()-st} ')
+    
+        for identity in resp:
+            LOGGER.info(f'image blurring started ')
+            st=time.time()
+            print(f'image blurring started - {st} ')
+            try:
+                facial_area = resp[identity]['facial_area']   
+                blurred=cv2.GaussianBlur(img,(71,71),500)
+
+            except KeyError as e:
+                # Handle KeyError (e.g., if 'facial_area' or 'identity' is not found in resp)
+                LOGGER.error(f'KeyError: {str(e)}')
+
+            except cv2.error as e:
+                # Handle OpenCV errors (e.g., if there's an issue with image processing)
+                LOGGER.error(f'OpenCV error: {str(e)}')
+
+            except Exception as e:
+                # Handle other unexpected errors
+                LOGGER.error(f'Unexpected error: {str(e)}')
+
+            try:     
+                # Get detected face area from retinaFace response and assign face region to face veriable
+                x1, y1, x2, y2 = facial_area
+            
+                face = img[y1:y2, x1:x2]
+
+                #blur the face region
+                
+                # Create circular mask of the same size as the blurred image
+                mask = np.zeros_like(blurred)
+                h, w = face.shape[:2]
+                center = ((x1+w//2), (y1+h//2))
+                
+                cv2.ellipse(mask,center,(w//2,h//2),0,0,360, (255, 255, 255), -1)
+                # cv2.circle(mask, center, radius, (255, 255, 255), -1)
+                # cv2.imwrite("mask.jpg",mask)
+                #create a circular blurred section
+                blurred_circular = cv2.bitwise_and(blurred, mask)
+                
+                
+                #apply the blurred circular section into the detected face area
+                blurredFace=np.where(mask!=0,blurred_circular,img)
+
+                #apply the blured face into the sorce image
+                img[y1:y2, x1:x2]=blurredFace[y1:y2, x1:x2]
+            
+            except cv2.error as e:
+                LOGGER.error(f"OpenCV error occurred: {e}")
+                # Handle specific OpenCV errors
+
+            except IndexError as e:
+                LOGGER.error(f"IndexError: {e}")
+                # Handle IndexError if facial_area doesn't have expected values
+
+            except Exception as e:
+                LOGGER.error(f"An unexpected error occurred: {e}")
+                # Handle any other unexpected errors   
+        # blurred_images.append(img)
+        # indexes.append(idx) 
+        if resp!={}:
+            LOGGER.info(f'image blurring ended for for image ')
+            print(f'image blurring started - {time.time()-st} ')
+            return img
+        else:
+            LOGGER.info(f'No faces detected')
+            print(f'No faces detected - {time.time()-st} ')
+            return None
+            
+        # if blurred_images !=[]:
+        #     return blurred_images,indexes
+        # else:
+        #     return None
+
+
+
+#   return image_base64
+# if __name__=='__main__':
+  
+#    blureFace_file()
+   
